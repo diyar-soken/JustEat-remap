ee84fea6c4c763da3c0411ec2cfa9d3f0b8a10dc
playboy  n <user@syn-172-254-010-148.biz.spectrum.com>
2025-06-09 15:35:10 +0200

    Initial commit

---
+++ src/main/java/it/diyar/justeatremap/JustEatRemapApplication.java
@@ -0,0 +1,13 @@
+package it.diyar.justeatremap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustEatRemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustEatRemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/diyar/justeatremap/JustEatRemapApplicationTests.java
@@ -0,0 +1,13 @@
+package it.diyar.justeatremap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustEatRemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
